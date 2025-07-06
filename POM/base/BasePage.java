9a819ef232872315525a20ee02ab4e967cd216bc
Omkar Gharat <83211956+OmkarGharat@users.noreply.github.com>
2025-07-06 14:18:31 +0530

    Updated BasePage.java

--- POM/base/BasePage.java
+++ POM/base/BasePage.java
@@ -1,1 +1,29 @@
+package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+public class BaseTest {
+
+	public WebDriver driver;
+
+	@BeforeClass
+	public void setup() {
+		System.out.println("Test Execution Started...");
+
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		System.out.println("Browser Lanuched...");
+	}
+
+	@AfterClass
+	public void tearDown() {
+		System.out.println("Test Execution Completed...");
+
+		driver.quit();
+	}
+
+}
